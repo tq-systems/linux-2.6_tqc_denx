6c0e883b88b31db9bb73421b9691fd0004bb08e4
John Rigby <jrigby@freescale.com>
2008-02-25 11:14:45 -0700

    MPC5121 hide pci bridge
    The class of the MPC5121 pci host bridge is PCI_CLASS_BRIDGE_OTHER
    while other freescale host bridges have class set to
    PCI_CLASS_PROCESSOR_POWERPC.
    
    This patch makes fixup_hide_host_resource_fsl match
    PCI_CLASS_BRIDGE_OTHER in addition to PCI_CLASS_PROCESSOR_POWERPC.
    
    Signed-off-by: John Rigby <jrigby@freescale.com>

--- arch/powerpc/kernel/pci_32.c
+++ arch/powerpc/kernel/pci_32.c
@@ -64,7 +64,7 @@ fixup_hide_host_resource_fsl(struct pci_dev* dev)
 {
 	int i, class = dev->class >> 8;
 
-	if ((class == PCI_CLASS_PROCESSOR_POWERPC) &&
+	if ((class == PCI_CLASS_PROCESSOR_POWERPC || class == PCI_CLASS_BRIDGE_OTHER) &&
 		(dev->hdr_type == PCI_HEADER_TYPE_NORMAL) &&
 		(dev->bus->parent == NULL)) {
 		for (i = 0; i < DEVICE_COUNT_RESOURCE; i++) {
