345dd90bda005c07448610246689fbb07d6c9e9e
York Sun <yorksun@freescale.com>
2008-02-29 15:42:05 -0600

    MPC5121 DIU separate platform functions
    Some functions are for compatible purpose only.
    
    Signed-off-by: York Sun <yorksun@freescale.com>

--- arch/powerpc/platforms/512x/mpc5121_ads.c
+++ arch/powerpc/platforms/512x/mpc5121_ads.c
@@ -23,8 +23,32 @@
 #include <asm/prom.h>
 #include <asm/time.h>
 
+#include <sysdev/fsl_soc.h>
+
 #include <linux/bootmem.h>
 #include <asm/rheap.h>
+
+#ifdef DEBUG
+#define DPRINTK(fmt, args...) printk("%s: " fmt,__FUNCTION__,## args)
+#else
+#define DPRINTK(fmt, args...)
+#endif
+
+static u32 get_busfreq(void)
+{
+	struct device_node *node;
+
+	u32 fs_busfreq=0;
+	node = of_find_node_by_type(NULL, "cpu");
+	if (node) {
+		unsigned int size;
+		const unsigned int *prop = of_get_property(node,"bus-frequency", &size);
+		if (prop)
+		fs_busfreq = *prop;
+		of_node_put(node);
+	};
+	return fs_busfreq;
+}
 
 #ifdef CONFIG_FB_FSL_DIU
 
@@ -33,6 +57,112 @@ static rh_info_t diu_rh_info;
 
 static unsigned long diu_size = 1280 * 1024 * 4; /* One 1280x1024 buffer */
 static void *diu_mem;
+
+unsigned int platform_get_pixel_format(unsigned int bits_per_pixel, int monitor_port)
+{
+	unsigned int pix_fmt;
+
+	if (bits_per_pixel == 32) {
+		pix_fmt = 0x88883316;
+	}
+	else if (bits_per_pixel == 24) {
+		pix_fmt = 0x88082219;
+	}
+	else if (bits_per_pixel == 16) {
+		pix_fmt = 0x65053118;
+	}
+	else
+		pix_fmt = 0x88883316;
+
+	return pix_fmt;
+}
+EXPORT_SYMBOL(platform_get_pixel_format);
+
+void platform_set_gamma_table(int monitor_port, char *gamma_table_base)
+{
+}
+EXPORT_SYMBOL(platform_set_gamma_table);
+
+void platform_set_monitor_port(int monitor_port)
+{
+}
+EXPORT_SYMBOL(platform_set_monitor_port);
+
+void platform_set_pixel_clock(unsigned int pixclock)
+{
+	u32 * clkdvdr, temp;
+	/* variables for pixel clock calcs */
+	unsigned long  bestval, bestfreq, speed_ccb, minpixclock, maxpixclock, pixval;
+	long err;
+	int i;
+
+	clkdvdr = (u32 *)ioremap(get_immrbase() + 0xf0c, sizeof(u32));
+
+	/* Pixel Clock configuration */
+	DPRINTK("DIU: Bus Frequency = %d\n",get_busfreq());
+	speed_ccb = get_busfreq() * 4;
+
+	/* Calculate the pixel clock with the smallest error */
+	/* calculate the following in steps to avoid overflow */
+	DPRINTK("DIU pixclock in ps - %d\n",pixclock);
+	temp = 1;
+	temp *= 1000000000;
+	temp /= pixclock;
+	temp *= 1000;
+	pixclock = temp;
+	DPRINTK("DIU pixclock freq - %lu\n",pixclock);
+
+	temp *= 5;
+	temp /= 100;  /* pixclock * 0.05 */
+	DPRINTK("deviation = %d\n", temp);
+	minpixclock = pixclock - temp;
+	maxpixclock = pixclock + temp;
+	DPRINTK("DIU minpixclock - %lu\n", minpixclock);
+	DPRINTK("DIU maxpixclock - %lu\n", maxpixclock);
+	pixval = speed_ccb/pixclock;
+	DPRINTK("DIU pixval = %lu\n",pixval);
+
+	err = 100000000;
+	bestval = pixval;
+	DPRINTK("DIU bestval = %lu\n", bestval);
+
+	bestfreq = 0;
+	for (i = -1; i <= 1; i++) {
+		temp = speed_ccb / (pixval+i);
+		DPRINTK("DIU test pixval i= %d, pixval=%lu, temp freq. = %u\n",i,pixval,temp);
+		if ((temp < minpixclock) || (temp > maxpixclock))
+			DPRINTK("DIU exceeds monitor range (%lu to %lu)\n",
+				minpixclock,maxpixclock);
+		else if (abs(temp - pixclock) < err) {
+		  DPRINTK("Entered the else if block %d\n", i);
+			err = abs(temp - pixclock);
+			bestval = pixval+i;
+			bestfreq = temp;
+		}
+	}
+
+	DPRINTK("DIU chose = %lx\n", bestval);
+	DPRINTK("DIU error = %ld\n NomPixClk ", err);
+	DPRINTK("DIU: Best Freq = %lx\n",bestfreq);
+	/* Modify PXCLK in GUTS CLKDVDR */
+	DPRINTK("DIU: Current value of CLKDVDR = 0x%08x\n",(*clkdvdr));
+	temp = (* clkdvdr) & 0xffffff00;
+	* clkdvdr = temp | (bestval & 0xFF);
+	DPRINTK("DIU: Modified value of CLKDVDR = 0x%08x\n",(*clkdvdr));
+}
+EXPORT_SYMBOL(platform_set_pixel_clock);
+
+ssize_t platform_show_monitor_port(int monitor_port, char * buf)
+{
+	return snprintf(buf, PAGE_SIZE, "0 - 5121ads DVI & LCD\n");
+}
+EXPORT_SYMBOL(platform_show_monitor_port);
+
+int platform_set_sysfs_monitor_port(int val)
+{
+	return 0;
+}
+EXPORT_SYMBOL(platform_set_sysfs_monitor_port);
 
 static void __init preallocate_diu_videomemory(void)
 {
@@ -51,7 +181,7 @@ static void __init preallocate_diu_videomemory(void)
 	rh_attach_region(&diu_rh_info, (unsigned long) diu_mem, diu_size);
 }
 
-void *fsl_diu_alloc(unsigned long size, dma_addr_t *phys)
+void *fsl_diu_alloc(unsigned long size, unsigned long *phys)
 {
 	 void *virt;
 
@@ -65,7 +195,7 @@ void *fsl_diu_alloc(unsigned long size, dma_addr_t *phys)
 	 if (virt)
 		*phys = virt_to_bus(virt);
 
-	 printk(KERN_DEBUG "%s:%u rh virt=%p phys=%x\n",
+	 printk(KERN_DEBUG "%s:%u rh virt=%p phys=%lx\n",
 			 __FUNCTION__, __LINE__, virt, *phys);
 
 	 return virt;
