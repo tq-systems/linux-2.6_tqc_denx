c27f32170b817051086f7ac0d0d0b69d84cbf57b
John Rigby <jrigby@freescale.com>
2008-04-28 13:21:44 -0600

    Fix PSCFIFO error bits
    Signed-off-by: John Rigby <jrigby@freescale.com>

--- include/asm-powerpc/mpc52xx_psc.h
+++ include/asm-powerpc/mpc52xx_psc.h
@@ -92,7 +92,6 @@
 
 #define MPC52xx_PSC_RFNUM_MASK	0x01ff
 
-
 /* Structure of the hardware registers */
 struct mpc52xx_psc {
 	u8		mode;		/* PSC + 0x00 */
@@ -198,8 +197,9 @@ struct mpc52xx_psc_fifo {
 #define MPC512x_PSC_FIFO_FULL		0x2
 #define MPC512x_PSC_FIFO_ALARM		0x4
 #define MPC512x_PSC_FIFO_URERR		0x8
-#define MPC512x_PSC_FIFO_ORERR		0x01
-#define MPC512x_PSC_FIFO_MEMERROR	0x02
+#define MPC512x_PSC_FIFO_ORERR		0x10
+#define MPC512x_PSC_FIFO_DATARDY	0x20
+#define MPC512x_PSC_FIFO_MEMERROR	0x40
 
 struct mpc512x_psc_fifo {
 	u32		reserved1[10];
